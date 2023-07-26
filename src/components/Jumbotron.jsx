c21e4037ff3fdafe5455c2cab55fe9886b897c5d
haroldCoder <haroldc2005@gmail.com>
2023-07-26 09:06:49 -0500

    remove canvas

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 
-function Model(props) {
-    const { scene } = useGLTF("src/assets/animations/scene0/scene.gltf");
-    return <primitive object={scene} {...props} />
-}
+// function Model(props) {
+//     const { scene } = useGLTF("src/assets/animations/scene0/scene.gltf");
+//     return <primitive object={scene} {...props} />
+// }
 
 export default function Jumbotron() {
   return (
@@ -24,14 +24,14 @@ export default function Jumbotron() {
                 <a className='link'>Learn more</a>
             </li>
         </ul>
-        <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{width: "30vw", height: "50vh"}}>
+        {/* <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{width: "30vw", height: "50vh"}}>
             <color attach="background" args={["#000"]} />
             <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}>
                 <Stage environment={"sunset"}>
                 <Model scale={0.01} rotation-y={2} />
                 </Stage>
             </PresentationControls>
-        </Canvas>
+        </Canvas> */}
     </div>
   )
 }
